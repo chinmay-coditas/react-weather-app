a1a517471265153840146baa0a8f6ee5f071a19b
Chinmay Patil <chinmay.patil@coditas.com>
2023-09-18 16:27:19 +0530

    refactor: Refactored code to use constants

---
+++ src/config.tsx
@@ -0,0 +1,9 @@
+export const ApiEndpoints = {
+  WEATHER_API: "weather",
+  AIR_POLLUTION_API: "air_pollution",
+};
+
+export const OpenWeatherApiKey = {
+  API_KEY: "7222d77720ff91f2c48972b4cb568221",
+};
+
--- src/services/openweather.service.tsx
+++ src/services/openweather.service.tsx
@@ -1,24 +1,31 @@
 import axios from "axios";
+import { ApiEndpoints, OpenWeatherApiKey } from "../config";
 
 export const getOpenWeatherData = async (
   latitude: string,
   longitude: string
 ) => {
   const { data } = await axios.get(
-    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    "https://api.openweathermap.org/data/2.5/" +
+      ApiEndpoints.WEATHER_API +
+      "?lat=" +
       latitude +
       "&lon=" +
       longitude +
-      "&units=metric&appid=7222d77720ff91f2c48972b4cb568221"
+      "&units=metric&appid=" +
+      OpenWeatherApiKey.API_KEY
   );
   return data;
 };
 
 export const getOpenWeatherDataByCityName = async (cityName: string) => {
   const { data } = await axios.get(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    "https://api.openweathermap.org/data/2.5/" +
+      ApiEndpoints.WEATHER_API +
+      "?q=" +
       cityName +
-      "&units=metric&appid=7222d77720ff91f2c48972b4cb568221"
+      "&units=metric&appid=" +
+      OpenWeatherApiKey.API_KEY
   );
   return data;
 };
@@ -28,11 +35,14 @@ export const getAirPollutionData = async (
   longitude: string
 ) => {
   const { data } = await axios.get(
-    "http://api.openweathermap.org/data/2.5/air_pollution?lat=" +
+    "http://api.openweathermap.org/data/2.5/" +
+      ApiEndpoints.AIR_POLLUTION_API +
+      "?lat=" +
       latitude +
       "&lon=" +
       longitude +
-      "&units=metric&appid=7222d77720ff91f2c48972b4cb568221"
+      "&units=metric&appid=" +
+      OpenWeatherApiKey.API_KEY
   );
   return data;
 };
